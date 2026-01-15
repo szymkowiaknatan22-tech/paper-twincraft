83c27a8a5ab469bd9f2ed29dbc0c5b6d98d412c5
szymkowiaknatan22-tech <szymkowiaknatan22@gmail.com>
2026-01-15 16:40:01 +0100

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "twincraft-v1"
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ])
+    )
+  )
+})
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  )
+})
